f67dea277b87f711d1f45002ece50f695c958ee0
jdbernardes <juliocd.bernardes@gmail.com>
2024-07-24 19:13:13 -0300

    (new_feature-extract_data.py) - Adding the first part of the ETL

---
+++ desafio_fake_etl/extract/extract_data.py
@@ -0,0 +1,23 @@
+import glob
+import os
+from typing import List
+
+import pandas as pd
+
+
+class ExtractData:
+
+    def __init__(self, data_dir: str) -> None:
+        self._data_dir = data_dir
+
+    def extract_data(self) -> List[pd.DataFrame]:
+        files: list = glob.glob(os.path.join(self._data_dir, "*.json"))
+        data_list: List[pd.DataFrame] = [pd.read_json(file) for file in files]
+        return data_list
+
+
+if __name__ == "__main__":
+    folder = "Data"
+    exdata = ExtractData(folder)
+    df_list = exdata.extract_data()
+    print(df_list[0].head())
