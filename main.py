baa2abe5c49abbdfde1a0c1af775810b73bc3bb9
Júlio Bernardes <juliocd.bernardes@gmail.com>
2024-07-26 21:47:51 -0300

    (main.py) - Creating main.py file

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from desafio_fake_etl.extract.extract_data import ExtractData
+from desafio_fake_etl.load.load_data import LoadData
 from desafio_fake_etl.transform.aggregate import Aggregate
 
 folder = "Data"
+extensions = ["csv", "parquet"]
+# extensions = ["parquet"]
+# extensions = ["csv"]
 edata = ExtractData(folder)
 aggdata = Aggregate()
+ldata = LoadData()
 df = aggdata.aggregate(edata.extract_data())
+ldata.load_data(extensions, df)
 print(df.head())
